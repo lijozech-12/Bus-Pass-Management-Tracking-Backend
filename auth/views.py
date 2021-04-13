2d72c444e4728981658858f0941808757a52e71f
Steev James <steev.james11@gmail.com>
2021-04-13 10:03:25 +0530

    Added Authentication

---
+++ auth/views.py
@@ -0,0 +1,50 @@
+# from django.shortcuts import render
+
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
+from rest_framework.response import Response
+
+from django.contrib.auth.models import User
+
+import requests
+
+
+# @csrf_exempt
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def login(request):
+    userData = requests.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo?access_token="+request.POST["accessToken"]).json()
+    # print(userData)
+    username = userData["email"]
+    password = "topsecretkey"
+    if username is None:
+        return Response({'error': 'Operation failed'}, status=HTTP_400_BAD_REQUEST)
+    user = None
+    if User.objects.filter(username=username).exists():
+        user = authenticate(username=username, password=password)
+        print("Logged in")
+    else:
+        user = User.objects.create_user(
+            username=username, email=username, password=password)
+        print("created new user")
+    if not user:
+        return Response({'error': 'Invalid Credentials'}, status=HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key}, status=HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(["GET"])
+def sample_api(request):
+    print(request.user)
+    data = {'response': "Authentication Success"}
+    return Response(data, status=HTTP_200_OK)
